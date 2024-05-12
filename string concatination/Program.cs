5e4ab1d1bbdc925524c0694983bc30886c2aba57
Bibhot1 <bndazbvu.me12@gmail.com>
2024-05-13 05:16:24 +1200

    Add project files.

---
+++ string concatination/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//making an addition calculator first.
+Console.WriteLine("Please enter your first number");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the second number");
+double num2 = double.Parse(Console.ReadLine());
+double result = num1 + num2;
+
+//using string interpolation to print out the value.
+Console.WriteLine($"the sum of {num1} and {num2} is {result}");
